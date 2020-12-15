933fa6c1ba720da51a34e3c96cd5fba6f2d0aa10
wendajiang <wendajiang93@163.com>
2020-12-16 00:24:29 +0800

    restore brew git respo

--- restore_brew.sh
+++ restore_brew.sh
@@ -7,7 +7,7 @@ git -C "$(brew --repo)" remote set-url origin https://github.com/Homebrew/brew.g
 git -C "$(brew --repo homebrew/core)" remote set-url origin https://github.com/Homebrew/homebrew-core.git
 git -C "$(brew --repo homebrew/cask)" remote set-url origin https://github.com/Homebrew/homebrew-cask.git
 git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://github.com/Homebrew/homebrew-cask-fonts.git
-git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://github.com/Homebrew/homebrew-cask-drivers.git
+# git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://github.com/Homebrew/homebrew-cask-drivers.git
 
 # 更换后测试工作是否正常
 brew update
